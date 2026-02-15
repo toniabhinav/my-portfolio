621e056eaf264cce91b85b26a33be85fe35d65dc
Abhinav <abhinavkumartk48@gmail.com>
2026-02-16 00:34:49 +0530

    Updated

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,8 @@ export default function Page() {
 
       {/* CURSOR SPOTLIGHT */}
       <motion.div
-        className="pointer-events-none fixed inset-0 -z-50"
+  className="pointer-events-none fixed top-0 left-0 w-full h-full -z-50"
+
         animate={{
           background: `radial-gradient(
             600px at ${mouse.x}px ${mouse.y}px,

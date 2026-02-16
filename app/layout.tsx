7d68ef770a769dde0c6d453d0ba317cec922a7c5
Abhinav <abhinavkumartk48@gmail.com>
2026-02-16 15:27:25 +0530

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
+
         {children}
       </body>
     </html>
